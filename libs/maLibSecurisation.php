98cc2a963f69b8123069309966db013366e42d17
DezelDev <dezeldev@gmail.com>
2026-05-26 00:03:05 +0200

    Add libs

---
+++ libs/maLibSecurisation.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once "maLibUtils.php";	// Car on utilise la fonction valider()
+
+/**
+ * Fonction à placer au début de chaque page privée
+ * Cette fonction redirige vers la page $urlBad en envoyant un message d'erreur 
+	et arrête l'interprétation si l'utilisateur n'est pas connecté
+ * Elle ne fait rien si l'utilisateur est connecté, et si $urlGood est faux
+ * Elle redirige vers urlGood sinon
+ */
+function securiser($urlBad,$urlGood=false)
+{
+	if (! valider("connecte","SESSION")) {
+		rediriger($urlBad);
+		die("");
+	}
+	else {
+		if ($urlGood)
+			rediriger($urlGood);
+	}
+}
+
+?>
