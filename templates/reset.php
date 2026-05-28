d0c9e435e6fbfb1ffb5eded12b0c571178545ea0
Wogooo <rpl2018@outlook.fr>
2026-05-28 08:45:48 +0200

    b

--- templates/reset.php
+++ templates/reset.php
@@ -12,14 +12,23 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
     <h1>Nouveau Mot de Passe</h1>
     <form role="form" action="controleur.php">
         <div class="form-group">
-            <input type="text" class="form-control" id="NewMdp" name="NewMdp" placeholder="Nouveau mot de passe">
+            <input type="text" class="input" id="NewMdp" name="NewMdp" placeholder="Nouveau mot de passe">
         </div>
         <div class="form-group">
-            <input type="text" class="form-control" id="ConfirmNewMdp" name="ConfirmNewMdp" placeholder="Confirmer nouveau mot de passe">
+            <input type="text" class="input" id="ConfirmNewMdp" name="ConfirmNewMdp" placeholder="Confirmer nouveau mot de passe">
         </div>
-        <p class="mmdp">Il est conseillé d'inventer une phrase unique et mémorable.</p>
+        <div id="notifications">
+			<div class="different">
+				<img src="./ressources/culturanked/invalid.png" alt="Invalide">
+				<p class="invalid">Les mots de passes sont différents</p>
+			</div>
+			<div class="advice">
+				<img src="./ressources/culturanked/information.png" alt="Info">
+				<p>Il est conseillé d'inventer une phrase unique et mémorable</p>
+			</div>
+		</div>
         <div>
-            <button type="submit" name="action" value="NouveauMdp" class="btn btn-default" id="valider">Valider</button>
+            <button type="submit" name="action" value="NouveauMdp" class="button secondary" id="valider">Valider</button>
         </div>
     </form>
 
