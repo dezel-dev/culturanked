1ab73e33f2c8c896ba7e0bd79ef85425475f06bf
DezelDev <dezeldev@gmail.com>
2026-05-28 15:33:10 +0200

    Update #2

--- libs/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$BDD_host="localhost";
-$BDD_user="rcleuet"; // nom d'utilisateur de phpmyadmin
-$BDD_password="Xukpg2007"; // mdp de phpmyadmin
-$BDD_base="culturanked"; // nom de la bdd
-
-?>
